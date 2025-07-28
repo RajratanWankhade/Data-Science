b0a56d02e8e8678f86f2eedcb1a47e4c34106017
Rajratan Wankhade <rajwnkhade9997@hotmail.com>
2025-07-28 15:57:19 +0200

    different project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'raj',
+    author_email= 'ratan.rajdevils@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
